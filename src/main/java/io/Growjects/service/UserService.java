4e7bca7e80ad0b36a8bc78cba0a0aa6038d8f6b0
Lucas-Azedo <2007.lucas.azevedo@gmail.com>
2025-07-15 23:22:42 -0300

    feat: GetUser routes

--- src/main/java/io/Growjects/service/UserService.java
+++ src/main/java/io/Growjects/service/UserService.java
@@ -4,8 +4,15 @@ import io.Growjects.model.dto.user.UserResponse;
 import io.Growjects.model.dto.user.UserSignRequest;
 import io.Growjects.model.entity.User;
 import io.Growjects.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +33,30 @@ public class UserService {
         res.setEmail(user.getEmail());
         res.setName(user.getName());
         res.setId(user.getId());
+        return res;
+    }
+
+    public UserResponse getUserById(UUID id){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+
+        return toUserResponse(user);
+    }
+
+    public List<UserResponse> getAllUsers(){
+        List<User> users = userRepository.findAll();
+
+        return users.stream().map(user ->{
+            return toUserResponse(user);
+        }).collect(Collectors.toList());
+    }
+
+    public UserResponse toUserResponse(User user){
+        UserResponse res = new UserResponse();
+        res.setId(user.getId());
+        res.setName(user.getName());
+        res.setEmail(user.getEmail());
+
         return res;
     }
 }
