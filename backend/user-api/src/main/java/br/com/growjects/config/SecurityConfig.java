90b34588c4fff73abc86f5a73d2b8aa44caff0d1
Lucas-Azedo <2007.lucas.azevedo@gmail.com>
2025-08-02 19:16:44 -0300

    feat: securityConfig

--- backend/user-api/src/main/java/br/com/growjects/config/SecurityConfig.java
+++ backend/user-api/src/main/java/br/com/growjects/config/SecurityConfig.java
@@ -2,10 +2,28 @@ package br.com.growjects.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> {})
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                );
+
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
